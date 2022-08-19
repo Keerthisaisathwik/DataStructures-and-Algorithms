77eaf0ac07a2f4ab95131bab6f502059b72d4401
Keerthi sai sathwik <96810503+Keerthisaisathwik@users.noreply.github.com>
2022-08-19 22:49:23 +0530

    Create Day4_Pangram_Checking.java
    Pangram Checking      ---gfg

---
+++ strings/Day4_Pangram_Checking.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial template for JAVA
+
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function template for JAVA
+
+class Solution
+{
+    //Function to check if a string is Pangram or not.
+    public static boolean checkPangram  (String str) {
+        // your code here
+        if(str.length()<26)
+            return false;
+        int arr[] = new int[26];
+        str = str.toLowerCase();
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)-'a'<26 && str.charAt(i)-'a'>=0)
+                arr[str.charAt(i)-'a']++;
+        }
+        for(int i: arr){
+            if(i==0)
+                return false;
+        }
+        return true;
+    }
+}
+
+//{ Driver Code Starts.
+
+class GFG
+ {
+	public static void main (String[] args) throws IOException
+	 {
+	  BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	  int t=Integer.parseInt(br.readLine());
+	  while(t-->0)
+	  {
+	    String s=br.readLine().trim();
+	    
+	    System.out.println(new Solution().checkPangram (s)==true?1:0);
+	  }
+	  
+	 }
+}
+// } Driver Code Ends
