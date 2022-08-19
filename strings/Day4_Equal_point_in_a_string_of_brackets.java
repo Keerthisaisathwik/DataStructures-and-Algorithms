0b6f1161cbe9f69bcd592e3e06db7a2dfa89f054
Keerthi sai sathwik <96810503+Keerthisaisathwik@users.noreply.github.com>
2022-08-19 22:25:42 +0530

    Create Day4_Equal_point_in_a_string_of_brackets.java
    Equal point in a string of brackets     ---gfg

---
+++ strings/Day4_Equal_point_in_a_string_of_brackets.java
@@ -0,0 +1,55 @@
+/*
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(N)
+*/
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            String str = br.readLine();
+            
+            Solution obj = new Solution();
+            System.out.println(obj.countSub(str));
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public long countSub(String str) {
+        // Your code goes here 
+        int close = 0;
+        int open = 0;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)==')')
+                open++;
+        }
+        if(close==open)
+            return 0;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)=='(')
+                close++;
+            else
+                open--;
+            if(close==open)
+                return i+1;
+        }
+        return str.length();
+    }
+}
