fcc38208f89ed27d1069c2ee964e2a7643dd043a
Keerthi sai sathwik <96810503+Keerthisaisathwik@users.noreply.github.com>
2022-08-20 20:02:02 +0530

    Create Day5_Remove_all_duplicates_from_a_given_string.java
    Remove all duplicates from a given string      ---gfg

---
+++ strings/Day5_Remove_all_duplicates_from_a_given_string.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            String str = br.readLine().trim();
+
+            String ans = new Solution().removeDuplicates(str);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    String removeDuplicates(String str) {
+        // code here
+        LinkedHashSet<Character> lhs = new LinkedHashSet<>();
+        for(int i=0;i<str.length();i++){
+            lhs.add(str.charAt(i));
+        }
+        StringBuffer sb = new StringBuffer();
+        for(Character ch : lhs)
+            sb.append(ch);
+        return sb.toString();
+    }
+}
+
