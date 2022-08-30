f0d457991b037797e847717802b6ecd79ef020ce
Keerthi sai sathwik <96810503+Keerthisaisathwik@users.noreply.github.com>
2022-08-30 23:03:44 +0530

    Create CoinChange.java
    coin change problem

---
+++ DP_ImportantQuestions/CoinChange.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class CoinChange {
+
+	public static void main(String[] args) {
+
+		int n = 18;
+		int coins[] = {7, 5, 1};
+		
+		int dp[] = new int[n+1];
+		Arrays.fill(dp, -1);
+		dp[0] = 0;
+		
+		int ans = minCoins(n, coins, dp);
+		System.out.println(ans);
+		
+		for(int x: dp) {
+			System.out.print(x+" ");
+		}
+	}
+	
+	static int minCoins(int n, int coins[], int dp[]) {
+		
+		if(n == 0) return 0;
+		
+		int ans = Integer.MAX_VALUE;
+		
+		for(int i = 0; i<coins.length; i++) {
+			if(n-coins[i] >= 0) {
+				int subAns = 0;
+				if(dp[n-coins[i]] != -1) {
+					subAns = dp[n-coins[i]];
+				} else {
+					subAns = minCoins(n-coins[i], coins, dp);
+				}
+				if(subAns != Integer.MAX_VALUE && 
+						subAns + 1 < ans) {
+					ans = subAns + 1;
+				}
+			}
+		}
+		return dp[n] = ans;
+	}
+}
