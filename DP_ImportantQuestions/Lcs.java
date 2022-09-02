72a4cb6547b92709c1859be672568f2caf8918d4
Keerthi sai sathwik <96810503+Keerthisaisathwik@users.noreply.github.com>
2022-09-02 22:41:33 +0530

    Update Lcs.java
    Dp

--- DP_ImportantQuestions/Lcs.java
+++ DP_ImportantQuestions/Lcs.java
@@ -1,53 +1,44 @@
-//{ Driver Code Starts
-//Initial Template for Java
 import java.io.*;
-import java.util.*;
-class GfG
+ 
+class GFG
 {
-    public static void main(String args[])
-        {
-            Scanner sc = new Scanner(System.in);
-            int t = sc.nextInt();
-            while(t-->0)
-                {
-                    String s1 = sc.next();
-                    String s2 = sc.next();
-                    Solution ob = new Solution();
-                    System.out.println(ob.minOperations(s1,s2));
-                }
-        }
-}    
-// } Driver Code Ends
-
-
-//User function Template for Java
-
-class Solution
-{
-	public int minOperations(String str1, String str2) 
-	{ 
-	    // Your code goes here
-	    int[][] dp = new int[str1.length()+1][str2.length()+1];
-	    for(int i=0;i<m + 1;i++){
-            for(int j=0;j<n + 1;j++){
-                dp[i][j] = -1;
-            }
-        }
-	    int lcsCount = 0;
-	    lcsCount = lcs(str1, str2, str1.length(), str2.length(), dp[][]);
-	    return str1.length()+ str2.length() - (2*lcsCount);
-	}
-	
-	public int lcs(String str1, String str2, int m, int n, int dp[][]){
-	    if(m==0 || n==0)
-	        return 0;
-	    if(dp[m][n]!=-1)
-	        return dp[m][n];
-	    if(str1.charAt(m-1)==str2.charAt(n-1)){
-	        result = 1+lcs(str1, str2, m-1, n-1, dp);
-	        return result;
-	    }
-	    else
-	        return dp[m][n] = result = Math.max(lcs(str1, str2, m-1, n, dp), lcs(str1, str2, m, n-1, dp));
+   
+  // A Top-Down DP implementation of LCS problem
+ 
+  // Returns length of LCS for X[0..m-1], Y[0..n-1]
+  static int lcs(String X,String Y,int m,int n,int[][] dp){
+ 
+    if (m == 0 || n == 0)
+      return 0;
+ 
+    if (dp[m][n] != -1)
+      return dp[m][n];
+ 
+    if(X.charAt(m - 1) == Y.charAt(n - 1)){
+      dp[m][n] = 1 + lcs(X, Y, m - 1, n - 1, dp);
+      return dp[m][n];
     }
+ 
+    dp[m][n] = Math.max(lcs(X, Y, m, n - 1, dp),lcs(X, Y, m - 1, n, dp));
+    return dp[m][n];
+  }
+ 
+  // Drivers code
+  public static void main(String args[]){
+ 
+    String X = "AGGTAB";
+    String Y = "GXTXAYB";
+ 
+    int m = X.length();
+    int n = Y.length();
+    int[][] dp = new int[m + 1][n + 1];
+    for(int i=0;i<m + 1;i++){
+      for(int j=0;j<n + 1;j++){
+        dp[i][j] = -1;
+      }
+    }
+ 
+    System.out.println("Length of LCS is "+lcs(X, Y, m, n, dp));
+ 
+  }
 }
