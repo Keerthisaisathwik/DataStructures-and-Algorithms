2a0e162e445eddeb62a8a42e005061bf9efced1b
Keerthi sai sathwik <96810503+Keerthisaisathwik@users.noreply.github.com>
2022-09-12 21:20:54 +0530

    Create Day20_Longest_Common_Subsequence.java
    Longest Common Subsequence      ---gfg

---
+++ 50_DP/Day20_Longest_Common_Subsequence.java
@@ -0,0 +1,27 @@
+
+
+class Solution
+{
+    //Function to find the length of longest common subsequence in two strings.
+    static int lcs(int n, int m, String S1, String S2)
+    {
+        // your code here
+        int dp[][] = new int[n+1][m+1];
+        for(int i=0;i<n+1;i++){
+            dp[i][0] = 0;
+        }
+        for(int i=1;i<m+1;i++){
+            dp[0][i] = 0;
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(S1.charAt(i-1)==S2.charAt(j-1))
+                    dp[i][j] = dp[i-1][j-1]+1;
+                else
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
+    }
+    
+}
