b3a803f6edd866c699fa36ae8e001af2e78e2677
Keerthi sai sathwik <96810503+Keerthisaisathwik@users.noreply.github.com>
2022-09-25 21:09:42 +0530

    Create Day10_Queries_on_a_Matrix.java
    Queries on a Matrix

---
+++ POTD/Day10_Queries_on_a_Matrix.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(s[0]);
+            int q = Integer.parseInt(s[1]);
+            int[][] Queries = new int[q][4];
+            for (int i = 0; i < q; i++) {
+                String[] s1 = br.readLine().trim().split(" ");
+                for (int j = 0; j < 4; j++)
+                    Queries[i][j] = Integer.parseInt(s1[j]);
+            }
+            Solution obj = new Solution();
+            int[][] ans = obj.solveQueries(n, Queries);
+            for (int i = 0; i < ans.length; i++) {
+                for (int j = 0; j < ans[i].length; j++) {
+                    out.print(ans[i][j] + " ");
+                }
+                out.println();
+            }
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int[][] solveQueries(int n, int[][] Queries) {
+        // Code here
+        int[][] arr=new int[n][n];
+        for(int[] q: Queries){
+            for(int i=q[0];i<=q[2];i++){
+                for(int j=q[1];j<=q[3];j++){
+                    arr[i][j]++;
+                }
+            }
+        }
+        return arr;
+    }
+}
+//solve it in O(n2)            ---> Easy
+//I have solved it in O(n3)    ---> Hard
