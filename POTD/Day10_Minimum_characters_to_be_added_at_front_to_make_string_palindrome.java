0fa71eac53a5deff608588a7fa16c434d04d49ad
Keerthi sai sathwik <96810503+Keerthisaisathwik@users.noreply.github.com>
2022-10-02 23:41:19 +0530

    Create Day10_Minimum_characters_to_be_added_at_front_to_make_string_palindrome.java
    Minimum characters to be added at front to make string palindrome     ---gfg

---
+++ POTD/Day10_Minimum_characters_to_be_added_at_front_to_make_string_palindrome.java
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Solution {
+    public static int minChar(String str) {
+       //Write your code here
+       int l=0,r=str.length()-1;
+        int temp=r;
+        int count=0;
+        while(l<r){
+            if(str.charAt(l)==str.charAt(r)){
+                l++;
+                r--;
+            }
+            else{
+                count++;
+                l=0;
+                r=--temp;
+            }
+        }
+        return count;
+    }
+}
+
+//{ Driver Code Starts.
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	    var sc = new FastReader();
+	    int test = sc.nextInt();
+	    while(test-->0){
+    		String s = sc.nextString();
+    		System.out.println(Solution.minChar(s));
+	    }
+	}
+    
+    public static class FastReader
+	{
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader()
+		{
+			br = new BufferedReader(new InputStreamReader(System.in));
+			st = new StringTokenizer("");
+		}
+
+		private void read() throws IOException
+		{
+			st = new StringTokenizer(br.readLine());
+		}
+
+		public String nextString() throws IOException
+		{
+			while(!st.hasMoreTokens())
+				read();
+			return st.nextToken();
+		}
+
+		public int nextInt() throws IOException
+		{
+			return Integer.parseInt(nextString());
+		}
+
+		public long nextLong() throws IOException
+		{
+			return Long.parseLong(nextString());
+		}
+	}
+}
+// } Driver Code Ends
